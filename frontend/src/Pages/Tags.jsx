bc2c8e5813166daa309e4abc161c9cd63d4eb9fd
Athul Vinod <80886537+DevAthul-88@users.noreply.github.com>
2022-02-12 19:45:50 +0530

    changes

--- frontend/src/Pages/Tags.jsx
+++ frontend/src/Pages/Tags.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
 
 function Tags() {
   return (
-    <div>Tags</div>
-  )
+    <div>
+      <h1 className="title has-text-weight-bold">Tags</h1>
+      <p className="subtitle mt-4 is-size-6">
+        A tag is a keyword or label that categorizes your question with other,
+        similar questions. Using the right tags makes it easier for others to
+        find and answer your question.
+      </p>
+      <div className="columns">
+        <div className="column">
+          <div className="field">
+            <p className="control has-icons-left">
+              <input
+                className="input"
+                type="text"
+                placeholder="Filter by tag name"
+              />
+              <span className="icon is-small is-left">
+                <i className="fa fa-search" aria-hidden="true"></i>
+              </span>
+            </p>
+          </div>
+        </div>
+        <div className="column">
+         
+        </div>
+      </div>
+
+        <div className="card-container is-flex ">
+        <div className="tag-card card is-shadowless p-5">
+          <div>
+          <div className="tag post_tag has-text-weight-bold">
+            Javascript
+          </div>
+         <div className="mt-2 ">
+         44 questions
+         </div>
+          </div>
+        </div>
+        </div>
+
+    </div>
+  );
 }
 
-export default Tags
+export default Tags;
