b6b5d51916d6595804cf6e6ab1b423672db763c2
Athul Vinod <80886537+DevAthul-88@users.noreply.github.com>
2022-02-18 16:29:00 +0530

    Config header

--- Controllers/quesCtrl.js
+++ Controllers/quesCtrl.js
@@ -4,18 +4,15 @@ const { v4: uuid } = require("uuid");
 
 module.exports = {
   create: async (req, res) => {
-    try {
-      const { tags } = req.body;
+    const { tags } = req.body;
 
-      const tag = await tags.map((e) => {
-        return {
-          name: e,
-        };
-      });
+    const tag = await tags.map((e) => {
+      return {
+        name: e,
+      };
+    });
 
-      await tagSchema.insertMany(tag);
-    } catch (error) {
-      res.json({ error: error.message });
-    }
+    await tagSchema.insertMany(tag);
+    res.json({status:true})
   },
 };
