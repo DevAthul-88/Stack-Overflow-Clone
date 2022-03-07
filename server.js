afbed16cf25cff39643c0aeb129c1300b0bcff2b
Athul Vinod <80886537+DevAthul-88@users.noreply.github.com>
2022-03-07 16:38:16 +0530

    make it better

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ const app = express();
 const mongoose = require("mongoose");
 const userRoute = require("./Routes/userRoute");
 const quesRouter = require("./Routes/quesRoute");
+const path = require("path")
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use("/api/user/" , userRoute)
-app.use("/api/question" , quesRouter)
+app.use("/api/user/", userRoute);
+app.use("/api/question", quesRouter);
 
 mongoose.connect(process.env.MONGO_DB_URL, (err) => {
   if (err) {
@@ -18,6 +19,13 @@ mongoose.connect(process.env.MONGO_DB_URL, (err) => {
   }
   console.log("Connection established");
 });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+  });
+}
 
 app.listen(process.env.PORT, () => {
   console.log(`Server listening on port ${process.env.PORT}`);
